b11840092d250a68f7dac6e57da5e563e2826d46
Cesar Vrgas <CesarVargas27@outlook.com>
2022-11-22 21:36:25 -0600

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,23 @@
 const API = 'https://youtube-music1.p.rapidapi.com/v2/search?query=Autumn%20Kings'
+let songsData = [];
+let totalSongs = 0;
 
 const content = null || document.getElementById('content');
+const nextButton = document.getElementById('nextButton');
+const returnButton = document.getElementById('returnButton');
+
+const div = document.createElement('div');
+div.classList += 'flex items-center justify-center';
+
+const loadingMsg = document.createElement('p');
+loadingMsg.className += 'block text-center text-3xl';
+loadingMsg.textContent = 'Loading songs...';
+
+div.appendChild(loadingMsg);
+
+let start = 0
+let limit = 4
+
 
 const options = {
 	method: 'GET',
@@ -18,13 +35,16 @@ async function fetchData(urlApi) {
 }
 
 
-( async() => {
+const getSongs = async( ) => {
     try {
         const songs = await fetchData(API);
+        songsData = songs;
+        totalSongs = Math.ceil(songs.result.songs.length);
+
         let view = 
         `
             ${songs.result.songs.map(song => `
-                <div class="group relative">
+                <div class="group">
                     <div
                         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
                         <img src="${song.thumbnail}" alt="Image_${song.name}" class="w-full">
@@ -35,13 +55,42 @@ async function fetchData(urlApi) {
                             ${song.name}
                         </h3>
                     </div>
+                    <a class="z-10" href="https://www.youtube.com/watch?v=${song.id}" target="_blank">Listen song</a>
                 </div>
-            `).slice(0, 4).join('')}
-            
+            `).slice(start, limit).join('')}
         `;
 
         content.innerHTML = view;
     } catch (error) {
         console.log(error);
     }
+}
+
+
+(() => {
+    content.appendChild(div);
+    getSongs();
 })()
+
+
+const nextSongs = () => {
+    if( totalSongs <= limit ) return;
+    start = start + 4;
+    limit = limit + 4
+    content.innerHTML = '';
+    content.appendChild(div);
+    getSongs();
+    
+} 
+
+const returntSongs = () => {
+    if( limit === 4 ) return;
+    start = start - 4;
+    limit = limit - 4
+    content.innerHTML = '';
+    content.appendChild(div);
+    getSongs();
+} 
+
+nextButton.addEventListener('click', nextSongs)
+returnButton.addEventListener('click', returntSongs)
